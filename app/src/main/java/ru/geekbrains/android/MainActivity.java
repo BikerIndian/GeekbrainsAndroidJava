1219a756e5c41e5d3d3117fbc18866c7a6b753a3
Vladimir Svishch <5693031@gmail.com>
2020-08-15 16:37:57 +0300

    hw-android-1-02

--- app/src/main/java/ru/geekbrains/android/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android/MainActivity.java
@@ -2,7 +2,10 @@ package ru.geekbrains.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setDefaultTemp();
+        setBtn();
+
+    }
+
+    private void setBtn() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClickedSelectCity();
+            }
+        });
+    }
+
+    private void onButtonClickedSelectCity() {
+        startActivity(new Intent(this, SelectCityActivity.class));
+    }
+
+
+    private void setDefaultTemp() {
         TextView textTemp = findViewById(R.id.text_temp);
         textTemp.setText("24");
-
     }
 }
