9fac22672b6da70bd519183553d4f6b93e344704
Vladimir Svishch <5693031@gmail.com>
2020-10-05 02:02:54 +0300

    hw-android-2-06 2. Настройте пуш-уведомления в вашем приложении и уведомляйте пользователей о штормовом предупреждении (о геолокации мы поговорим позже).

---
+++ app/src/main/java/ru/geekbrains/android/FirebaseService.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.android;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseService extends FirebaseMessagingService {
+    private int messageId = 0;
+
+    private  String TAG = "PushMessageAndroid";
+    public FirebaseService() {
+        Log.d(TAG, "FirebaseMessaging =" + FirebaseMessaging.getInstance().getToken().toString());
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, remoteMessage.getNotification().getBody());
+        String title = remoteMessage.getNotification().getTitle();
+        if (title == null){
+            title = "Push Message";
+        }
+        String text = remoteMessage.getNotification().getBody();
+        // создать нотификацию
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "2")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(text);
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(messageId++, builder.build());
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        // Если надо посылать сообщения этому экземпляру приложения
+        // или управлять подписками прилоения на стороне сервера,
+        // сохраните этот токен в базе данных. отправьте этот токен вашему
+        Log.d(TAG, "Token " + token);
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+    }
+}
